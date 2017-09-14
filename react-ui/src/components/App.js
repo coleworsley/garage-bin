1cec91d2b0b69111bf955e04bd078200e311e486
coleworsley <cole.s.worsley@gmail.com>
2017-09-14 14:13:21 -0600

    add post feature

--- react-ui/src/components/App.js
+++ react-ui/src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import GarageDoor from './GarageDoor';
 import InputContainer from './InputContainer';
 import List from './List';
+import { GARAGE_ENDPOINT } from '../utils/constants';
+import { getItems } from '../helpers';
 
 const initialState = {
   items: [],
@@ -12,27 +14,37 @@ class App extends Component {
   constructor() {
     super();
     this.state = initialState;
+    this.postItem = this.postItem.bind(this);
   }
 
+  postItem(body) {
+    fetch(GARAGE_ENDPOINT, {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify(body),
+    })
+    .then(res => res.json())
+    .then(item => {
+      const { items } = this.state;
+      this.setState({ items: [...items, item], error: '' })
+    })
+    .catch((error) => {
+      this.setState({ error });
+    })
+  };
 
   componentDidMount() {
-    fetch('/api/v1/garage')
-      .then(res => res.json())
-      .then(items => {
-        this.setState({ items, error: '' })
-      })
-      .catch(error => {
-        this.setState({ error })
-      })
+    getItems(this);
   }
+
   render() {
     return (
       <div className="App">
         <h1>Hello World</h1>
         <main>
           <GarageDoor />
-          <InputContainer />
-          <List items={this.state.items}/>
+          <InputContainer postItem={this.postItem} />
+          <List items={this.state.items} />
         </main>
 
       </div>
--- react-ui/src/components/InputContainer.js
+++ react-ui/src/components/InputContainer.js
@@ -4,27 +4,41 @@ export default class InputContainer extends Component {
   constructor() {
     super();
     this.state = {
-      nameInput: '',
-      descriptionInput: '',
-      cleanlinessInput: '',
+      name: '',
+      description: '',
+      cleanliness: 'Sparkling',
     }
   }
 
   handleChange(e) {
-    this.setState({ cleanlinessInput: e.targe.value })
+    this.setState({ [e.target.name]: e.target.value })
   }
 
   render() {
     return (
       <div className='input-container'>
-        <input id='name-input' type='text' placeholder='name'/>
-        <input id='description-input' type='text' placeholder='description'/>
-        <select id='cleanliness-input'>
-          <option value='Sparkling'>Sparkling</option>
-          <option value='Dusty'>Dusty</option>
-          <option value='Rancid'>Rancid</option>
+        <input id='name-input'
+          type='text'
+          placeholder='name'
+          name='name'
+          value={this.state.nameInput}
+          onChange={(e) => this.handleChange(e)}/>
+        <input id='description-input'
+          type='text'
+          placeholder='description'
+          name='description'
+          value={this.state.descriptionInput}
+          onChange={(e) => this.handleChange(e)}/>
+        <select id='cleanliness-input'
+          name='cleanliness'
+          onChange={(e) => this.handleChange(e)}>
+            <option value='Sparkling'>Sparkling</option>
+            <option value='Dusty'>Dusty</option>
+            <option value='Rancid'>Rancid</option>
         </select>
-        <button id='submit-input'></button>
+        <button id='submit-input' onClick={() => this.props.postItem(this.state)}>
+          Submit
+        </button>
       </div>
     )
   }
