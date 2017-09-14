8c51cc883c4fdcd8931d36a6ee0e6bc57f04c4df
coleworsley <cole.s.worsley@gmail.com>
2017-09-14 10:21:26 -0600

    add seed file

--- db/seeds/dev/garage.js
+++ db/seeds/dev/garage.js
@@ -1,13 +1,48 @@
+const items = [
+  {
+    id: 1,
+    name: 'Cooler',
+    description: 'it cools things',
+    cleanliness: 'Sparkling',
+  },
+  {
+    id: 2,
+    name: 'Old Paint Cans',
+    description: 'Instead of stashing it in the house, you decide to get it out of sight.',
+    cleanliness: 'Dusty',
+  },
+  {
+    id: 3,
+    name: 'Garden Hose',
+    description: 'No matter how you store it, it\'s one of the most useful items you can own',
+    cleanliness: 'Sparkling',
+  },
+  {
+    id: 4,
+    name: 'Spiders',
+    description: 'Yeah... theres lots everywhere',
+    cleanliness: 'Rancid',
+  },
+  {
+    id: 5,
+    name: 'Dustpan',
+    description: 'Really who actually cleans there garage?',
+    cleanliness: 'Dusty',
+  },
+  {
+    id: 6,
+    name: 'Tools',
+    description: 'Yeah like you were going to use them',
+    cleanliness: 'Dusty',
+  },
+]
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+  return knex('garage')
+    .del()
+    .then(() => {
+      return Promise.all(
+        items.map(item => knex('garage').insert(item))
+      )
     });
 };
