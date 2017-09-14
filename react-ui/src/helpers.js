7a883b692a372a85a082b3021a3e0e92aa281543
coleworsley <cole.s.worsley@gmail.com>
2017-09-14 14:14:06 -0600

    update constants and helpers

--- react-ui/src/helpers.js
+++ react-ui/src/helpers.js
@@ -1,3 +1,34 @@
-export const getItems = () => {};
-export const postItem = () => {};
-export const updateItem = () => {};
+import { GARAGE_ENDPOINT } from './utils/constants';
+
+export const getItems = (component) => {
+  fetch(GARAGE_ENDPOINT)
+    .then(res => res.json())
+    .then(items => {
+      component.setState({ items, error: '' })
+    })
+    .catch(error => {
+      component.setState({ error })
+    })
+};
+
+export const postItem = (component, body) => {
+  fetch(GARAGE_ENDPOINT, {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'POST',
+    body: JSON.stringify(body),
+  })
+  .then(res => res.json())
+  .then(item => {
+    const { items } = component.state;
+    const newState = [...items].push(item);
+
+    component.setState({ items: newState, error: '' })
+  })
+  .catch((error) => {
+    component.setState({ error })
+  })
+};
+
+export const updateItem = () => {
+  // fetch(GARAGE_ENDPOINT)
+};
